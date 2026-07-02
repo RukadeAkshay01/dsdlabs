e5e0dedd57658e1694e45f375393c17a377c0804
Akshay Rukade <akshayrukade01@gmail.com>
2026-07-02 12:31:58 +0530

    Add up_down input for bidirectional counter

--- 03-pins/counter.sv
+++ 03-pins/counter.sv
@@ -1,6 +1,7 @@
 module counter (
     input logic clkpulse,       // Clock input
     input logic rst,         // Active low reset
+    input logic up_down,
     output logic [3:0] led // 4-bit counter output
 );
 
@@ -8,7 +9,11 @@ module counter (
         if (rst) begin
             led <= 4'b0000; // Reset counter to 0
         end else begin
+            if(up_down) begin
             led <= led + 1; // Increment counter
+            end else begin
+                led<= led-1;
+            end 
         end
     end
 
